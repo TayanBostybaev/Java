72fd68064ccec29345886417aba749cf2bcc3610
Tayan <bostybaev@list.ru>
2021-08-22 12:33:12 +0600

    commit1

---
+++ src/Calculator.java
@@ -0,0 +1,53 @@
+
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        startCalc();
+
+        while (true) {
+
+            System.out.println("Input: ");
+            String line = scanner.nextLine();
+
+            if (line.equals("exit")) {
+                exitCalc();
+                break;
+            }
+
+            try {
+                String[] symbols = line.split(" ");
+                
+                if (symbols.length != 3) throw new Exception("Формат операции не удовлетворяет заданию");
+
+                Number firstNumber = NumberService.parseAndValidate(symbols[0]);
+                Number secondNumber = NumberService.parseAndValidate(symbols[2], firstNumber.getType());
+                String result = ActionService.calculate(firstNumber, secondNumber, symbols[1]);
+                System.out.println("Output: \n" + result);
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());                
+                exitCalc();
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void startCalc() {
+        System.out.println("Данный Калькулятор работает только с арабскими и римскими цифрами от 1 до 10");
+        System.out.println("На не можно выполнить только следующие операции:");
+        System.out.println("Сложение(+), Вычитание(-), Умножение(*), Деление(/)");
+        System.out.println("Если Вы хотите покинуть программу, введите 'exit'");
+    }
+
+    private static void exitCalc() {
+
+        System.out.println("До скорых встреч!");
+
+    }
+}
